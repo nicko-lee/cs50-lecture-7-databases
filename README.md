ef9c6a6837083206208048f8396bf18372e2de30
nicko <nicko.lee@hotmail.com>
2020-09-29 03:28:59 +0000

    Write a Python script to let a user search the data
    - Also update a couple of README files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cs50-lecture-7-databases
+
+Key topics of this lecture include:
+1. Using a form to collect data live into a Google Spreadsheet
+2. Interrogating the data we collected with Python
+3. Intro to SQL - a better way to interrogate the data
+4. Intro to sqlite and creating our first SQL DB and making some queries
+5. Using a far bigger dataset - the IMDb list of 7M+ titles
---
+++ using-imdb-dataset/search.py
@@ -0,0 +1,15 @@
+import csv
+
+# ask user for input
+title = input("Title: ")
+
+# open our filtered CSV (a significantly smaller subset than the original IMDb file)
+# feel free to swap between shows0.csv or shows1.csv
+with open("shows1.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    # iterate over every row (over 10,000 rows) and check each row's title
+    for row in reader:
+
+        if title == row["primaryTitle"]:
+            print(row["primaryTitle"], row["startYear"])
